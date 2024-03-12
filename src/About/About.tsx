6d0bae9008ed45bd4d2c710ea5e286108a2010ad
Saikrishna <saikrishna.tammi064@gmail.com>
2024-03-12 06:47:43 +0530

    config changes updated and deployed

---
+++ src/About/About.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./About.scss";
+
+export const About: React.FC = () => {
+  return (
+    <div className="about">
+      <h5>About</h5>
+      <p>
+        GM, I'm Saikrishna. I enjoy building dynamic, creative products from
+        start to finish. Focused on developing intuitive experiences that
+        constantly grow and improve based on user metrics.
+      </p>
+    </div>
+  );
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import man from "../assets/man.png";
+import { About } from "./About/About";
 import "./App.scss";
 
 export const App: React.FC = () => {
   return (
     <div className="app">
       <div className="profile">
-        <h1 className="info">Saikrishna Tammi</h1>
-        <h6>FullStack Developer</h6>
+        <div className="profilePic">
+          <img src={man} alt="saikrishna" />
+        </div>
+        <div className="naming">
+          <h1 className="info">Saikrishna Tammi</h1>
+          <h6>FullStack Developer</h6>
+        </div>
+      </div>
+      <div>
+        <About />
       </div>
     </div>
   );
